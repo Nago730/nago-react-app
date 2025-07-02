9d23ac39ccb1162e816a1e8b36242faac194f06f
DESKTOP-4N4R3LH\jqk17 <jqk1797@gmail.com>
2025-07-02 23:56:04 +0900

    feat: shuffleArray 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import './App.css';
 import { useState } from 'nago-react';
 
+function shuffleArray(array: any[]) {
+	for (let i = array.length - 1; i > 0; i--) {
+	  const j = Math.floor(Math.random() * (i + 1));
+	  [array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+  }
+
 function App() {
 	const [count, setCount] = useState(0);
 	const [open, setOpen] = useState(false);
